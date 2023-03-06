22cd1892067f426b9fbc040521de3a99b6120786
Maxime Chouzy <maxime.chouzypro@gmail.com>
2023-03-06 11:50:00 +0100

    First commit

---
+++ renom-mass/frm_main.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace renom_mass
+{
+    partial class frm_main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnImporter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_nombe = new System.Windows.Forms.Label();
+            this.btn_rename = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(570, 184);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnImporter
+            // 
+            this.btnImporter.Location = new System.Drawing.Point(12, 202);
+            this.btnImporter.Name = "btnImporter";
+            this.btnImporter.Size = new System.Drawing.Size(167, 29);
+            this.btnImporter.TabIndex = 1;
+            this.btnImporter.Text = "Ajouter des fichiers";
+            this.btnImporter.UseVisualStyleBackColor = true;
+            this.btnImporter.Click += new System.EventHandler(this.btnImporter_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(588, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre Fichier :";
+            // 
+            // lbl_nombe
+            // 
+            this.lbl_nombe.AutoSize = true;
+            this.lbl_nombe.Location = new System.Drawing.Point(712, 12);
+            this.lbl_nombe.Name = "lbl_nombe";
+            this.lbl_nombe.Size = new System.Drawing.Size(50, 20);
+            this.lbl_nombe.TabIndex = 3;
+            this.lbl_nombe.Text = "label2";
+            // 
+            // btn_rename
+            // 
+            this.btn_rename.Location = new System.Drawing.Point(12, 237);
+            this.btn_rename.Name = "btn_rename";
+            this.btn_rename.Size = new System.Drawing.Size(167, 29);
+            this.btn_rename.TabIndex = 4;
+            this.btn_rename.Text = "Renomer";
+            this.btn_rename.UseVisualStyleBackColor = true;
+            this.btn_rename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // frm_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 280);
+            this.Controls.Add(this.btn_rename);
+            this.Controls.Add(this.lbl_nombe);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnImporter);
+            this.Controls.Add(this.listBox1);
+            this.Name = "frm_main";
+            this.Text = "frm_main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Button btnImporter;
+        private Label label1;
+        private Label lbl_nombe;
+        private Button btn_rename;
+    }
+}
---
+++ renom-mass/frm_main.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace renom_mass
+{
+    public partial class frm_main : Form
+    {
+        public frm_main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImporter_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = true;
+            int t = 0;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                // Ajouter les fichiers sélectionnés à la liste
+                foreach (string fichier in dialog.FileNames)
+                {
+                    listBox1.Items.Add(fichier);
+                    t++;
+                }
+                lbl_nombe.Text = t.ToString();
+            }
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            // Vérifier que l'utilisateur a sélectionné un fichier dans la liste
+            if (listBox1.SelectedIndex != -1)
+            {
+                // Ouvrir une boîte de dialogue pour demander le nouveau nom de fichier
+                string nouveauNom = Microsoft.VisualBasic.Interaction.InputBox("Entrez le nouveau nom de fichier", "Renommer le fichier", listBox1.SelectedItem.ToString());
+
+                // Vérifier que l'utilisateur a entré un nom de fichier valide
+                if (nouveauNom.Trim() != "")
+                {
+                    // Renommer le fichier
+                    string ancienNom = listBox1.SelectedItem.ToString();
+                    string nouveauChemin = Path.GetDirectoryName(ancienNom) + "\\" + nouveauNom;
+
+                    try
+                    {
+                        File.Move(ancienNom, nouveauChemin);
+                        listBox1.Items[listBox1.SelectedIndex] = nouveauChemin;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Une erreur est survenue lors du renommage du fichier: " + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Veuillez sélectionner un fichier à renommer.");
+            }
+        }
+
+        private void btnRenommerAuto_Click(object sender, EventArgs e)
+        {
+            // Vérifier que l'utilisateur a sélectionné un dossier dans le contrôle FolderBrowserDialog
+            if (fbdDossier.ShowDialog() == DialogResult.OK)
+            {
+                // Demander à l'utilisateur de saisir le format de nom de fichier
+                string format = Microsoft.VisualBasic.Interaction.InputBox("Entrez le format de nom de fichier (utilisez {0} pour représenter le numéro de séquence)", "Renommer automatiquement les fichiers");
+
+                // Vérifier que l'utilisateur a entré un format valide
+                if (format.Trim() != "")
+                {
+                    // Récupérer la liste des fichiers dans le dossier sélectionné
+                    string[] fichiers = Directory.GetFiles(fbdDossier.SelectedPath);
+
+                    // Définir le compteur de séquence
+                    int compteur = 1;
+
+                    // Parcourir tous les fichiers dans le dossier
+                    foreach (string fichier in fichiers)
+                    {
+                        // Obtenir l'extension du fichier
+                        string extension = Path.GetExtension(fichier);
+
+                        // Construire le nouveau nom de fichier
+                        string nouveauNom = string.Format(format, compteur) + extension;
+                        string nouveauChemin = Path.GetDirectoryName(fichier) + "\\" + nouveauNom;
+
+                        try
+                        {
+                            File.Move(fichier, nouveauChemin);
+                            compteur++;
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Une erreur est survenue lors du renommage du fichier: " + ex.Message);
+                        }
+                    }
+
+                    // Actualiser la liste des fichiers dans la ListBox
+                    listBox1.Items.Clear();
+                    listBox1.Items.AddRange(Directory.GetFiles(fbdDossier.SelectedPath));
+                }
+            }
+        }
+
+
+
+
+    }
+}
